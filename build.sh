e62c775fd60f0ea0c99316b0c272be052c06626e
gstaaij <gstaaij@protonmail.com>
2024-01-22 20:15:59 +0100

    Update build.sh to use the build folder

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -xe
 
-gcc -Wall -Wextra -o diffiehellman diffiehellman.c
-x86_64-w64-mingw32-gcc -Wall -Wextra -o diffiehellman diffiehellman.c
+gcc -Wall -Wextra -o build/diffiehellman diffiehellman.c
+x86_64-w64-mingw32-gcc -Wall -Wextra -o build/diffiehellman diffiehellman.c
